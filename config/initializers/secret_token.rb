d722ca9b3a450b4045c4e6668f39fd6a0ab0afe7
geoverge <kannabiran.shinestar@gmail.com>
2013-07-26 11:52:33 +0530

    Firat commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b1c50b1551d22bb181ea81fa8bb8a07256a4f1bb03a6e89d3a3acecf41addc2086eb284afaf3decba560524a87ef5169db01f157b21b499c7546b72a8679758e'
